7069adc70935fa455ca2fe1bcf5bec35b850ea69
Pratham vichare <prathamvichare645@gmail.com>
2025-01-06 00:50:25 -0500

    Data ingestion done

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.DSProject import logger
+from src.DSProject.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("custom logging DSProject")
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.initiate_data_ingestion()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
