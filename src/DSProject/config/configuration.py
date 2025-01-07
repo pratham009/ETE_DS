9846fe0635bd2b0d99b8c22293439f75c8f1a847
Pratham vichare <prathamvichare645@gmail.com>
2025-01-07 01:17:29 -0500

    Data Validation done

--- src/DSProject/config/configuration.py
+++ src/DSProject/config/configuration.py
@@ -1,6 +1,6 @@
 from src.DSProject.constants import *
 from src.DSProject.utils.common import read_yaml, create_directories
-from src.DSProject.entity.config_entity import (DataIngestionConfig)
+from src.DSProject.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(self,
@@ -9,7 +9,7 @@ class ConfigurationManager:
                  schema_filepath = SCHEMA_FILE_PATH):
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
-        self.filepath = read_yaml(schema_filepath)
+        self.schema = read_yaml(schema_filepath)
 
         create_directories([self.config.artifacts_root])
 
@@ -26,3 +26,18 @@ class ConfigurationManager:
 
         )
         return data_ingestion_config
+    
+    def get_data_validation(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
